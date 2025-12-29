aa18539d3b6d08deaabce639214f9de91d659d15
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-12-29 23:02:59 +0100

    refactor: remove unused import

--- src/views/layout.php
+++ src/views/layout.php
@@ -10,7 +10,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet">
  
-    <link rel="stylesheet" href="/styles/variables.css">
     <link rel="stylesheet" href="/styles/main.css">
 </head>
 <body>
