21819aaa6c7d2f16354154c90740d75fadb8312a
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-12-30 00:32:25 +0100

    feat: register login, dashboard, logout

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../src/core/Env.php';
 require_once __DIR__ . '/../src/core/Routing.php';
 require_once __DIR__ . '/../src/core/Database.php';
 require_once __DIR__ . '/../src/controllers/AuthController.php';
+require_once __DIR__ . '/../src/controllers/DashboardController.php';
 
 
 Env::load(__DIR__ . '/../.env');
@@ -12,6 +13,10 @@ $router = Routing::getInstance();
 
 $router->get('/register', [new AuthController(), 'register']);
 $router->post('/register', [new AuthController(), 'register']);
+$router->get('/login', [new AuthController(), 'login']);
+$router->post('/login', [new AuthController(), 'login']);
+$router->get('/logout', [new AuthController(), 'logout']);
+$router->get('/dashboard', [new DashboardController(), 'index']);
 
 $path = $_SERVER['REQUEST_URI']; 
 $router->run($_SERVER['REQUEST_METHOD'], $path);
