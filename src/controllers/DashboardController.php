68daaa023ff844452fd36a4b42a9c39398692810
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2025-12-30 00:31:51 +0100

    feat(dashboard): add dashboard controller

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -1,72 +1,23 @@
 <?php
 
 require_once 'AppController.php';
-require_once __DIR__.'/../repository/UserRepository.php';
+
+
 
 class DashboardController extends AppController {
-    public function index(?int $id) {
-            error_log("DashboardController::index called with id: " . ($id ?? 'null'));
 
-    $cards = [
-        [
-            'id' => 1,
-            'title' => 'Ace of Spades',
-            'subtitle' => 'Legendary card',
-            'imageUrlPath' => 'https://deckofcardsapi.com/static/img/AS.png',
-            'href' => '/cards/ace-of-spades'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Queen of Hearts',
-            'subtitle' => 'Classic romance',
-            'imageUrlPath' => 'https://deckofcardsapi.com/static/img/QH.png',
-            'href' => '/cards/queen-of-hearts'
-        ],
-        [
-            'id' => 3,
-            'title' => 'King of Clubs',
-            'subtitle' => 'Royal strength',
-            'imageUrlPath' => 'https://deckofcardsapi.com/static/img/KC.png',
-            'href' => '/cards/king-of-clubs'
-        ],
-        [
-            'id' => 4,
-            'title' => 'Jack of Diamonds',
-            'subtitle' => 'Sly and sharp',
-            'imageUrlPath' => 'https://deckofcardsapi.com/static/img/JD.png',
-            'href' => '/cards/jack-of-diamonds'
-        ],
-        [
-            'id' => 5,
-            'title' => 'Ten of Hearts',
-            'subtitle' => 'Lucky draw',
-            'imageUrlPath' => 'https://deckofcardsapi.com/static/img/0H.png',
-            'href' => '/cards/ten-of-hearts'
-        ],
-    ];
+    public function index() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
 
-    if ($id != null) {
-        $cards = [
-            [
-                'id' => 1,
-                'title' => 'Ace of Spades',
-                'subtitle' => 'Legendary card',
-                'imageUrlPath' => 'https://deckofcardsapi.com/static/img/AS.png',
-                'href' => '/cards/ace-of-spades'
-            ],
-        ];
-    }
+        if (!isset($_SESSION['user_id'])) {
+            $this->redirect('/login');
+            return;
+        }
 
-        error_log("Cards array created with " . count($cards) . " items");
+        $roleId = $_SESSION['role_id'];
 
-
-        $userRepository = new UserRepository();
-        $users = $userRepository->getUsers();
-
-        var_dump($users);
-
-        return $this->render("dashboard", [
-            "cards" => $cards
-        ]);
+        $this->render('dashboard', ['role_id' => $roleId]);
     }
 }
