16b543e44aca51c15e9e2c8d6a44f27f9559d0e1
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2026-01-04 18:12:29 +0100

    feat(books): add add-book to index

--- public/index.php
+++ public/index.php
@@ -28,6 +28,9 @@ $router->get('/profile', [new ProfileController(), 'index']);
 $router->post('/loan/renew', [new ProfileController(), 'renewLoan']);
 $router->post('/reservation/cancel', [new ProfileController(), 'cancelReservation']);
 $router->post('/reservation/create', [new ProfileController(), 'createReservation']);
+$router->get('/add-book', [new BookController(), 'add']);
+$router->post('/add-book', [new BookController(), 'add']);
+
 
 $path = $_SERVER['REQUEST_URI']; 
 $router->run($_SERVER['REQUEST_METHOD'], $path);
