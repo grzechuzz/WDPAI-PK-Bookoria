0d5ed4052f90dbe0f9683386668020b23b7c9db1
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2026-01-05 15:52:36 +0100

    feat(auth): add csrf protection

--- src/views/auth/login.php
+++ src/views/auth/login.php
@@ -1,3 +1,4 @@
+<?php require_once __DIR__ . '/../../core/Csrf.php'; ?>
 <section class="login-wrapper">
     <article class="card">
         
@@ -20,6 +21,8 @@
         <?php endif; ?>
 
         <form action="/login" method="POST">
+            <?= Csrf::field() ?>
+            
             <div class="form-group">
                 <label for="email">Email</label>
                 <input type="email" id="email" name="email" placeholder="email@example.com" required>
