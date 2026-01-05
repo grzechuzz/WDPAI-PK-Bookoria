b9cbad99deb9b1cbaf8080b079df314c18151979
Grzegorz Rzepiela <grzegorz.rzepiela@student.pk.edu.pl>
2026-01-05 15:41:31 +0100

    chore: add httponly flag to cookie

--- public/index.php
+++ public/index.php
@@ -14,6 +14,11 @@ require_once __DIR__ . '/../src/controllers/CirculationController.php';
 
 Env::load(__DIR__ . '/../.env');
 
+session_set_cookie_params([
+    'httponly' => true,
+    'samesite' => 'Strict',
+]);
+
 session_start();
 
 $router = Routing::getInstance();
